e49fe958e26a7d9b5ad567465055547673a7219c
Alexey Sumin <sumin.200ok@gmail.com>
2021-06-22 21:45:06 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,52 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	var result strings.Builder
+	esc := '\\'
+	slice := []rune(str)
+	last := len(slice) - 1
+
+	for i := 0; i <= last; i++ {
+		char := slice[i]
+
+		// мы не должны никогда попадать на цифру, если предыдущий символ не экранируюший
+		if unicode.IsDigit(char) && (i == 0 || slice[i-1] != esc) {
+			return "", ErrInvalidString
+		}
+
+		// если последний символ, прибавляем его и выходим
+		if i == last {
+			result.WriteRune(char)
+			break
+		}
+
+		next := slice[i+1]
+
+		switch {
+		case next == esc: // следующий символ экранирующий
+			result.WriteRune(char)
+			i++
+
+		case unicode.IsDigit(next): // следующий символ цифра
+			num, err := strconv.Atoi(string(next))
+			if err != nil {
+				return "", err
+			}
+			result.WriteString(strings.Repeat(string(char), num))
+			i++
+
+		default: // следующий символ не цифра и не экранирующий
+			result.WriteRune(char)
+		}
+	}
+
+	return result.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -16,11 +16,10 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
-		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
 	}
 
 	for _, tc := range tests {
@@ -40,6 +39,27 @@ func TestUnpackInvalidString(t *testing.T) {
 		t.Run(tc, func(t *testing.T) {
 			_, err := Unpack(tc)
 			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
+		})
+	}
+}
+
+func TestMultiByteUnpack(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{input: "♬2Ё4丕2", expected: "♬♬ЁЁЁЁ丕丕"},
+		{input: "♬0丕", expected: "丕"},
+		{input: `丕\2Ё`, expected: "丕2Ё"},
+		{input: `♬0Ё0`, expected: ""},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.input, func(t *testing.T) {
+			result, err := Unpack(tc.input)
+			require.NoError(t, err)
+			require.Equal(t, tc.expected, result)
 		})
 	}
 }
