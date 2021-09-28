4efc715b685d016b1564ab57977c696e380e81a0
Alexey Sumin <sumin.200ok@gmail.com>
2021-09-28 19:50:41 +0300

    HW10 is completed

--- hw10_program_optimization/stats_test.go
+++ hw10_program_optimization/stats_test.go
@@ -16,6 +16,14 @@ func TestGetDomainStat(t *testing.T) {
 {"Id":4,"Name":"Gregory Reid","Username":"tButler","Email":"5Moore@Teklist.net","Phone":"520-04-16","Password":"r639qLNu","Address":"Sunfield Park 20"}
 {"Id":5,"Name":"Janice Rose","Username":"KeithHart","Email":"nulla@Linktype.com","Phone":"146-91-01","Password":"acSBF5","Address":"Russell Trail 61"}`
 
+	dataEmpty := ``
+
+	dataWithoutEmail := `{"Id":1,"Name":"Howard Mendoza","Username":"0Oliver","Phone":"6-866-899-36-79","Password":"InAQJvsq","Address":"Blackbird Place 25"}
+{"Id":2,"Name":"Jesse Vasquez","Username":"qRichardson","Phone":"9-373-949-64-00","Password":"SiZLeNSGn","Address":"Fulton Hill 80"}`
+
+	dataInvalidJson := `{"Email":"aliquid_qui_ea@Browsedrive.gov"}
+{"Email""mLynch@broWsecat.com"}`
+
 	t.Run("find 'com'", func(t *testing.T) {
 		result, err := GetDomainStat(bytes.NewBufferString(data), "com")
 		require.NoError(t, err)
@@ -35,5 +43,23 @@ func TestGetDomainStat(t *testing.T) {
 		result, err := GetDomainStat(bytes.NewBufferString(data), "unknown")
 		require.NoError(t, err)
 		require.Equal(t, DomainStat{}, result)
+	})
+
+	t.Run("find in empty", func(t *testing.T) {
+		result, err := GetDomainStat(bytes.NewBufferString(dataEmpty), "com")
+		require.NoError(t, err)
+		require.Equal(t, DomainStat{}, result)
+	})
+
+	t.Run("find without emails", func(t *testing.T) {
+		result, err := GetDomainStat(bytes.NewBufferString(dataWithoutEmail), "com")
+		require.NoError(t, err)
+		require.Equal(t, DomainStat{}, result)
+	})
+
+	t.Run("find into invalid json", func(t *testing.T) {
+		result, err := GetDomainStat(bytes.NewBufferString(dataInvalidJson), "com")
+		require.Error(t, err)
+		require.Nil(t, result)
 	})
 }
