a3e62ee404c1d01d47dde893992d582b8241723e
Alexey Sumin <sumin.200ok@gmail.com>
2021-10-12 19:13:01 +0300

    HW11 is completed

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,54 @@
 package main
 
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	timeout := flag.Duration("timeout", 10*time.Second, "connection timeout, default is 10s")
+	flag.Parse()
+	host := flag.Arg(0)
+	port := flag.Arg(1)
+	if host == "" || port == "" {
+		log.Fatal("host and port is required params")
+	}
+	addr := net.JoinHostPort(host, port)
+
+	c := NewTelnetClient(addr, *timeout, os.Stdin, os.Stdout)
+	if err := c.Connect(); err != nil {
+		log.Fatal(err)
+	}
+	defer c.Close()
+
+	io.WriteString(os.Stderr, fmt.Sprintf("...connected to: %s\n", addr))
+
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	go func() {
+		err := c.Receive()
+		if err != nil {
+			io.WriteString(os.Stderr, fmt.Sprintf("...%s\n", err))
+		}
+		cancel()
+	}()
+
+	go func() {
+		err := c.Send()
+		if err != nil {
+			io.WriteString(os.Stderr, fmt.Sprintf("...%s\n", err))
+		}
+		cancel()
+	}()
+
+	<-ctx.Done()
 }
